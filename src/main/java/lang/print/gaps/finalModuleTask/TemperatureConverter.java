9eb20a9c17f75cf0ed8477040ac1e5afe03e37fc
M <M>
2025-01-26 11:51:14 +0200

    ++

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double fahrenheit = (temperatureCelsius * 1.8) + 32;
+        double fahrenheit = (double) (temperatureCelsius * 1.8) + 32;
     }
 }
